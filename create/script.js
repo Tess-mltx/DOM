74bf50f92f02ac2af0ffa41c8580d2b21f6ca330
Tess <tess@DESKTOP-9F1581K>
2023-12-12 16:14:14 +0100

    3 DONE !

--- create/script.js
+++ create/script.js
@@ -1,22 +1,34 @@
 let learner = ["Josh", "Thom", "Marvyn", "Mathias", "Huseyin", "Giuseppe", "Aurélien", "Alexis", "Ugur", "Noé", "Alice", "Jeremy" ];
+let newLearner = learner.sort((a, b) => 0.5 - (Math.random()));
 
-function SectionLerner() {
-    
-    learner.forEach(element  => {
+function SectionLearner() {
+    newLearner.forEach(element  => {
         let x = document.querySelector("article");
         let y = document.createElement("section");
         let z = document.createElement("p");
-
 
         x.appendChild(y);
         y.appendChild(z);
         z.appendChild(document.createTextNode(element));
 
-        y.style.backgroundColor = RandomColor()
+        let h = (Math.floor(Math.random()*(360)));
+        let s = (Math.floor(Math.random()*(100)));
+        let l = (Math.floor(Math.random()*(100)));
+        let colorBg = ("hsl(" + h + ", " + s + "%, " + l + "%)");
+
+
+        y.style.backgroundColor = colorBg;
+
+        if(l < 50){
+            z.style.color = "hsl(" + h + ", " + s + "%, " + 85 + "%)";
+        } else {
+            z.style.color = "hsl(" + h + ", " + s + "%, " + 15 + "%)";
+        };
         
-    })
+        
+    })    
 }
-SectionLerner();
+SectionLearner();
 
 function RandomColor(){
     let r = (Math.floor(Math.random()*(255)));
@@ -24,4 +36,5 @@ function RandomColor(){
     let b = (Math.floor(Math.random()*(255)));
     return("rgb(" + r + ", " + g + ", " + b + ")");
 };
+
 
