4c2a1d45488e00fe0e9e11aeba9887010b29bc81
Tess <tess@DESKTOP-9F1581K>
2023-12-15 16:07:58 +0100

    confirm pwd border red

--- forms/script.js
+++ forms/script.js
@@ -6,7 +6,7 @@ let pwdConfirmInput = document.getElementById('pwd-confirm');
 let toggleDarkMode = document.getElementById('toggle-darkmode');
 
 firstnameInput.addEventListener("keyup", (e) => {
-    console.log('touche', e.key)
+    //console.log('touche', e.key)
     let displayFirstname = document.getElementById('display-firstname');
     if (e.key === 'Backspace') {
         let content = displayFirstname.textContent;
@@ -18,13 +18,36 @@ firstnameInput.addEventListener("keyup", (e) => {
 });
 
 ageInput.addEventListener("keyup", (e) => {
-    console.log('touche', e.key)
+    //console.log('touche', e.key);
+    let hardTruth = document.getElementById('a-hard-truth')
+    let ageValue = ageInput.value
+    //console.log(ageValue)
+    if (parseInt(ageValue) >= 18) {
+        hardTruth.style.visibility = "initial";
+    } else { 
+        hardTruth.style.visibility = "hidden";
+    }
 });
 
-pwdInput.addEventListener("keyup", (e) => {
-    console.log('touche', e.key)
-});
 
-pwdConfirmInput.addEventListener("keyup", (e) => {
-    console.log('touche', e.key)
-});
+const pwdVerification = () => {
+    console.log(pwdInput.value)
+    let pwd = pwdInput.value
+    let pwdConfirm = pwdConfirmInput.value
+
+    if (pwd.length < 6) {
+        pwdInput.style.borderColor = "red";
+    } else {
+        pwdInput.style.borderColor = "";
+        pwdConfirmInput.style.borderColor = "";
+    }
+
+    if (pwdConfirm !== pwd && pwdConfirm.length > 0) {
+        pwdConfirmInput.style.borderColor = "red";
+        pwdInput.style.borderColor = "red";
+    } else {
+        pwdConfirmInput.style.borderColor = "";
+    }
+}
+pwdInput.addEventListener("keyup", pwdVerification );
+pwdConfirmInput.addEventListener("keyup", pwdVerification );
